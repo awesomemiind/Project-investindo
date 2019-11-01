de92592b3fbc4702c58fca0ba45668b8c20f450e
Awesome Miind <spamawesomemiind@gmail.com>
2019-10-31 22:00:55 -0300

    Registro de usuario pronto

--- resources/views/templates/menu-lateral.blade.php
+++ resources/views/templates/menu-lateral.blade.php
@@ -1,7 +1,7 @@
 <nav id="principal">
     <ul>
         <li>
-            <a href="#">
+        <a href="{{ route('user.index') }} ">
                 <i class="fa fa-address-book"></i>
                 <h3>Usuarios</h3>
             </a>
